fb4f50883504a6b6a946d7d30a49fbb9dd853027
dangphuc282 <dangphuc28022002@gmail.com>
2025-06-17 10:21:45 +0900

    Add files via upload

---
+++ BTTH1/b2/main.js
@@ -0,0 +1,33 @@
+let student = {
+id : 1,
+name: "Nguyen Van A",
+gender: "nam",
+age: 20,
+mark: 8
+};
+
+let newStudent = {
+  id: 2,
+  name: "Tran Thi B",
+  gender: "nu",
+  age: 22,
+  mark: 9
+}
+
+let students = [student, newStudent];
+console.log("Doi tuong new student");
+
+console.log(`ID:`, students[1].id);
+console.log(`name:`, students[1].name);
+console.log(`Gioi Tinh:`, students[1].gender);
+console.log(`Tuoi:`, students[1].age);
+console.log(`Diem:`, students[1].mark);
+
+
+
+
+
+
+
+
+
