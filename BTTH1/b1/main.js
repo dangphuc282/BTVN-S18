3b68219293510a7e70c8cf7b0680db8597f6e918
dangphuc282 <dangphuc28022002@gmail.com>
2025-06-17 10:07:45 +0900

    Add files via upload

---
+++ BTTH1/b1/main.js
@@ -0,0 +1,14 @@
+let person = {
+name: "nguyen van A",
+age: 25,
+adress: "Ha noi",
+phone: "0123456789",
+};
+
+console.log(`Ten`, person.name);
+console.log((`Tuoi`, person.age));
+console.log(`Dia chi`, person.adress);
+console.log(`Dien thoai`, person.phone);
+
+
+
